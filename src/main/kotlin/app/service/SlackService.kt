fcdcf87ef155026de01c474d7003161b12245828
김형욱 <kho5420@gmail.com>
2022-11-16 18:05:13 +0900

    slack properties 생성

--- src/main/kotlin/app/service/SlackService.kt
+++ src/main/kotlin/app/service/SlackService.kt
@@ -2,16 +2,27 @@ package app.service
 
 import org.springframework.stereotype.Service
 import com.slack.api.Slack
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.PropertySource
+import org.springframework.stereotype.Component
 
+@Component
+@PropertySource("classpath:slack.properties")
 @Service
 class SlackService {
+    @Value("\${slack.token}")
+    lateinit var token: String
+
+    @Value("\${slack.channel}")
+    lateinit var channel: String
+
     fun sendMessage(): String{
         val client = Slack.getInstance().methods()
 
         runCatching {
             client.chatPostMessage {
-                it.token("xoxb-2137822082532-4375778787557-22q0AICB96dQkvW34hqE62ai")
-                    .channel("D04B75DT3J8")
+                it.token(token)
+                    .channel(channel)
                     .text("Are you mazeltov?")
             }
         }.onFailure { e ->
