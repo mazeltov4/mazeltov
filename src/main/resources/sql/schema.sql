f3e3c862007656eba974fabc1ddca49b990c9fde
김형욱 <kho5420@gmail.com>
2022-12-05 13:53:58 +0900

    스키마파일 수정(foreign key)

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,77 +1,114 @@
-CREATE TABLE organization
+create table authority
 (
-    id         BIGINT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT '조직 id',
-    parents_id BIGINT UNSINGED COMMENT '상위 조직 id',
-    depth      INT UNSINGED NOT NULL COMMENT '조직 레벨',
-    name       VARCHAR(32)         NOT NULL COMMENT '조직 이름',
-    use_yn     CHAR(2) DEFAULT 'N' NOT NULL COMMENT '조직 사용 여부'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='조직도';
+    id   bigint unsigned auto_increment comment '권한 id'
+        primary key,
+    type varchar(16) not null comment '권한 타입'
+)
+    comment '권한';
 
-
-CREATE TABLE organization_work_type
+create table organization
 (
-    id              INT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT '조직 출퇴근 유형 PK',
-    work_type_id    INT UNSIGNED NOT NULL COMMENT '출퇴근 유형 id',
-    organization_id BIGINT UNSINGED NOT NULL COMMENT '조직 id'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='조직 출퇴근 유형';
+    id         bigint unsigned auto_increment comment '조직도 id'
+        primary key,
+    parents_id bigint unsigned     not null comment '상위 조직 id',
+    depth      int unsigned        not null comment '조직 레벨',
+    name       varchar(32)         not null comment '조직이름',
+    use_yn     char(2) default 'N' not null comment '사용여부'
+)
+    comment '조직도';
 
-
-CREATE TABLE work_type
+create table user
 (
-    id       INT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT '출퇴근 유형 id',
-    name     VARCHAR(32) NOT NULL COMMENT '출퇴근 유형 이름',
-    on_time  VARCHAR(32) COMMENT '출근 시간',
-    off_time VARCHAR(32) COMMENT '퇴근 시간'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='출퇴근 유형';
+    id              bigint unsigned auto_increment comment '유저 id'
+        primary key,
+    organization_id bigint unsigned                          not null comment '조직 id',
+    authority_id    bigint unsigned                          not null comment '권한 번호',
+    email           varchar(256)                             not null comment '이메일',
+    birth           date                                     not null comment '생년월일',
+    phone           char(32)                                 not null comment '휴대폰 번호',
+    status          varchar(8)                               not null comment '유저 상태',
+    position        varchar(8)                               not null comment '직책',
+    start_work      datetime(6)                              not null comment '입사일',
+    end_work        datetime(6)                              null comment '퇴사일',
+    address         varchar(256)                             not null comment '주소',
+    created_at      datetime(6) default CURRENT_TIMESTAMP(6) not null comment '생성일',
+    updated_at      datetime(6) default CURRENT_TIMESTAMP(6) not null on update CURRENT_TIMESTAMP(6) comment '수정일',
+    constraint user_authority_id_fk
+        foreign key (authority_id) references authority (id),
+    constraint user_organization_id_fk
+        foreign key (organization_id) references organization (id)
+)
+    comment '유저';
 
-
-CREATE TABLE authority
+create table attendance
 (
-    id   BIGINT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT '권한 id',
-    type VARCHAR(16) NOT NULL COMMENT '권한 타입',
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='권한';
+    id         bigint unsigned auto_increment comment '출퇴근 id'
+        primary key,
+    user_id    bigint unsigned                          not null comment '유저 id',
+    clock_time datetime                                 not null comment '출퇴근 날짜',
+    ip         varchar(20)                              null comment 'ip',
+    location   varchar(200)                             null comment '위치',
+    memo       varchar(200)                             null comment '메모',
+    device     varchar(10)                              null comment '출근 디바이스',
+    created_at datetime(6) default CURRENT_TIMESTAMP(6) not null comment '생성일',
+    updated_at datetime(6) default CURRENT_TIMESTAMP(6) not null on update CURRENT_TIMESTAMP(6) comment '수정일',
+    constraint attendance_user_id_fk
+        foreign key (user_id) references user (id)
+)
+    comment '출퇴근';
 
-
-CREATE TABLE user
+create table user_vacation
 (
-    id              BIGINT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT '유저 id',
-    organization_id BIGINT UNSIGNED NOT NULL COMMENT '조직 id',
-    authority_id    INT UNSIGNED NOT NULL COMMENT '권한 id',
-    email           VARCHAR(256) NOT NULL COMMENT '이메일',
-    birth           DATE         NOT NULL COMMENT '생년월일',
-    phone           VARCHAR(32)  NOT NULL COMMENT '휴대폰 번호',
-    status          VARCHAR(8)   NOT NULL COMMENT '유저 상태',
-    position        VARCHAR(8)   NOT NULL COMMENT '유저 직급',
-    start_work      DATETIME(6) NOT NULL COMMENT '유저 입사일',
-    end_work        DATETIME(6) COMMENT '유저 퇴사일',
-    address         VARCHAR(256) NOT NULL COMMENT '유저 주소',
-    created_at      DATETIME(6) NOT NULL DEFALT CURRENT_TIMESTAMP (6) COMMENT '생성일자',
-    updated_at      DATETIME(6) DEFAULT CURRENT_TIMESTAMP (6) ON UPDATE CURRENT_TIMESTAMP (6) NOT NULL COMMENT '수정 일자'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='유저';
+    id          bigint unsigned auto_increment comment '휴가 id'
+        primary key,
+    user_id     bigint unsigned                          not null comment '유저 id',
+    count       float                                    not null comment '부여 휴가 일수',
+    expiry_date date                                     not null comment '만료일',
+    type        varchar(16)                              not null comment '연차 종류',
+    paid        char(2)     default 'N'                  not null comment '유급 휴가 여부',
+    remain      float                                    not null comment '남은 휴가 일수',
+    used_yn     char(2)     default 'N'                  null comment '사용완료 여부',
+    created_at  datetime(6) default CURRENT_TIMESTAMP(6) not null comment '생성일',
+    updated_at  datetime(6) default CURRENT_TIMESTAMP(6) not null on update CURRENT_TIMESTAMP(6) comment '수정일',
+    constraint user_vacation_user_id_fk
+        foreign key (user_id) references user (id)
+)
+    comment '연차';
 
-
-
-CREATE TABLE user_vacation
+create table user_vacation_log
 (
-    id          BIGINT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT '휴가 id',
-    user_id     BIGINT UNSIGNED NOT NULL COMMENT '유저 id',
-    expiry_date DATE                NOT NULL COMMENT '만료일',
-    type        VARCHAR(16)         NOT NULL COMMENT '휴가 종류',
-    paid        CHAR(2) DEFAULT 'N' NOT NULL COMMENT '유급 휴가 여부',
-    remain      FLOAT               NOT NULL COMMENT '남은 휴가 일수',
-    count       FLOAT               NOT NULL COMMENT '부여 휴가 일수',
-    used_yn     CHAR(2) DEFALT 'N' NOT NULL '사용 완료 여부',
-    created_at  DATETIME(6) NOT NULL DEFALT CURRENT_TIMESTAMP (6) COMMENT '생성일자',
-    updated_at  DATETIME(6) DEFAULT CURRENT_TIMESTAMP (6) ON UPDATE CURRENT_TIMESTAMP (6) NOT NULL COMMENT '수정 일자'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='휴가';
+    id               bigint unsigned auto_increment comment '휴가 로그 id'
+        primary key,
+    user_vacation_id bigint unsigned                          not null comment '휴가 id',
+    status           varchar(8)  default 'W'                  not null comment '휴가 상태 (W: 대기, P: 승인, F:반려, C:취소)',
+    date             date        default (curdate())          not null comment '날짜',
+    created_at       datetime(6) default CURRENT_TIMESTAMP(6) not null comment '생성일',
+    updated_at       datetime(6) default CURRENT_TIMESTAMP(6) not null on update CURRENT_TIMESTAMP(6) comment '수정일',
+    constraint user_vacation_log_user_vacation_id_fk
+        foreign key (user_vacation_id) references user_vacation (id)
+)
+    comment '연차 로그';
 
-
-CREATE TABLE user_vacation_log
+create table work_type
 (
-    id               BIGINT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT '휴가 로그 id',
-    user_vacation_id BIGINT UNSIGNED NOT NULL COMMENT '휴가 id',
-    status           VARCHAR(8)    DEFAULT 'W' NOT NULL COMMENT '휴가 상태(W:대기, P:승인, F:반려, C:취소)',
-    use_date         DATE NOT NULL DEFAULT CURRENT_DATE COMMENT '휴가 일자',
-    created_at       DATETIME(6) NOT NULL DEFALT CURRENT_TIMESTAMP (6) COMMENT '생성일자',
-    updated_at       DATETIME(6) DEFAULT CURRENT_TIMESTAMP (6) ON UPDATE CURRENT_TIMESTAMP (6) NOT NULL COMMENT '수정 일자'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='휴가 로그';
+    id       int unsigned auto_increment comment '출퇴근 유형 id'
+        primary key,
+    name     varchar(20) not null comment '출퇴근 유형 이름',
+    on_time  varchar(32) null comment '출근 시간',
+    off_time varchar(32) null comment '퇴근시간'
+)
+    comment '출퇴근 유형';
+
+create table organization_work_type
+(
+    id              int unsigned auto_increment comment '조직 출퇴근 유형 id'
+        primary key,
+    work_type_id    int unsigned    not null comment '출퇴근 유형 id',
+    organization_id bigint unsigned not null comment '조직도 id',
+    constraint organization_work_type_organization_id_fk
+        foreign key (organization_id) references organization (id),
+    constraint organization_work_type_work_type_id_fk
+        foreign key (work_type_id) references work_type (id)
+)
+    comment '조직 출퇴근 유형';
+
