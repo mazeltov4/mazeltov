2e79b370610355a9190ae8b0b33e482bdc020194
김형욱 <kho5420@gmail.com>
2022-12-07 10:20:04 +0900

    interactivity modal 팝업 추가

--- src/main/kotlin/app/controller/SlackController.kt
+++ src/main/kotlin/app/controller/SlackController.kt
@@ -17,22 +17,22 @@ class SlackController(
 ) {
     @PostMapping("/user", consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
     fun userCommand(@RequestBody body: MultiValueMap<String, String>){
-        val channelId = body["channel_id"]?.get(0)
-
-        channelId ?.let {
-            slackService.sendMessage(channelId, "user")
-        }
+        val channelId = body.getFirst("channel_id").toString()
+        slackService.sendMessage(channelId, "user")
     }
 
     @PostMapping("/interactivity", consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
     fun userInteractivity(@RequestBody body: MultiValueMap<String, String>){
         val payload = JSONObject(valueOf(body.getFirst("payload")))
+        val triggerId = payload.getString("trigger_id")
         val actionValue = payload.getJSONArray("actions").getJSONObject(0).getString("value")
         val channelId = payload.getJSONObject("channel").getString("id")
 
         when(actionValue) {
             "signup" -> {
-                slackService.sendMessage(channelId, "signup")
+                slackService.openModal(triggerId, actionValue)
+            } else -> {
+                return
             }
         }
     }
--- src/main/kotlin/app/service/SlackService.kt
+++ src/main/kotlin/app/service/SlackService.kt
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Service
 import com.slack.api.Slack
 import com.slack.api.model.block.LayoutBlock
 import com.slack.api.model.kotlin_extension.block.withBlocks
+import com.slack.api.model.kotlin_extension.view.blocks
+import com.slack.api.model.view.View
+import com.slack.api.model.view.Views.*
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.PropertySource
 import org.springframework.stereotype.Component
@@ -15,19 +18,30 @@ class SlackService {
     @Value("\${slack.token}")
     lateinit var token: String
 
-    fun sendMessage(channelId: String, type: String = ""): String{
+    fun sendMessage(channelId: String, type: String = "") {
         val client = Slack.getInstance().methods(token)
-        val response = client.chatPostMessage { req -> req
+        client.chatPostMessage { req -> req
             .channel(channelId)
             .blocks(
-                    when(type) {
+                when(type) {
                     "user" -> {userCommandBlock()}
                     else -> {userCommandBlock()}
                 }
             )
         }
+    }
 
-        return "success"
+    fun openModal(triggerId: String, type: String = "") {
+        val client = Slack.getInstance().methods(token)
+        client.viewsOpen{it
+            .triggerId(triggerId)
+            .view(
+                when(type) {
+                    "signup" -> {signupView()}
+                    else -> {signupView()}
+                }
+            )
+        }
     }
 
     fun userCommandBlock(): List<LayoutBlock> {
@@ -49,20 +63,67 @@ class SlackService {
         }
     }
 
-    fun signupBlock(): List<LayoutBlock>{
-        return withBlocks {
-            section {
-                markdownText("*직원 신규 등록*")
-            }
-            divider()
-            actions {
-                button {
-                    text("신규 등록", emoji = true)
-                    value("signup")
+    fun signupView(): View {
+        // ---------- 임시 리스트 ---------------------
+        val orgList = listOf("조직1", "조직2", "조직3", "조직4", "조직5")
+        // ---------- 임시 리스트 ---------------------
+
+        return view { thisView -> thisView
+            .callbackId("signup-modal")
+            .type("modal")
+            .notifyOnClose(true)
+            .title(viewTitle { it.type("plain_text").text("Meeting Arrangement").emoji(true) })
+            .submit(viewSubmit { it.type("plain_text").text("Submit").emoji(true) })
+            .close(viewClose { it.type("plain_text").text("Cancel").emoji(true) })
+            .blocks {
+                input {
+                    plainTextInput {
+                        actionId("option1")
+                        placeholder("작성")
+                    }
+                    label("제목 타이틀 여기1", emoji = true)
                 }
-                button {
-                    text("퇴사 처리", emoji = true)
-                    value("withdrawal")
+                divider()
+                input {
+                    plainTextInput {
+                        actionId("option2")
+                        placeholder("작성")
+                    }
+                    label("제목 타이틀 여기2", emoji = true)
+                }
+                divider()
+                section {
+                    markdownText("dropdown list 선택하기")
+                    accessory {
+                        staticSelect {
+                            placeholder("조직도 선택")
+                            options {
+                                for (orgName in orgList) {
+                                    option {
+                                        plainText(orgName)
+                                        value(orgName)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                divider()
+                section {
+                    markdownText("dropdown list 선택하기")
+                    accessory {
+                        staticSelect {
+                            placeholder("권한 선택")
+                            options {
+                                for (orgName in orgList) {
+                                    option {
+                                        plainText(orgName)
+                                        value(orgName)
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
